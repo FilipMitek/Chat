b448eb3754702fd0050d1246ca7391ee6ffd853f
Filip <Filip.Mitek@gmail.com>
2019-04-23 23:12:22 +0200

    updated config for typescript

--- server/index.ts
+++ server/index.ts
@@ -4,14 +4,14 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
+let config = require('../nuxt.config.ts')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
   // Build only in dev mode
   if (config.dev) {
@@ -22,10 +22,10 @@ async function start() {
   }
 
   // Give nuxt middleware to express
-  app.use(nuxt.render)
+  app.use(nuxt.render);
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
